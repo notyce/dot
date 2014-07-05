984da896f512e23493d5fd37f7166c2dfd2a9eab
shm <notyce@gmail.com>
2014-07-05 12:52:41 +0200

    use vim as default EDITOR

--- system/env.zsh
+++ system/env.zsh
@@ -1,5 +1,3 @@
-# Only set this if we haven't set $EDITOR up somewhere else previously.
 if [[ "$EDITOR" == "" ]] ; then
-  # Use mate for my editor.
-  export EDITOR='mate'
+  export EDITOR='vim'
 fi
