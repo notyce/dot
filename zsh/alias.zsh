d87c2e8a1eb2beae53a18184e933318bd456851b
shm <notyce@gmail.com>
2015-07-14 21:53:32 +0200

    zsh aliases update

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,3 +1,12 @@
 alias ls=' ls'
 alias cd=' cd'
+# Colorize output, add file type indicator, and put sizes in human readable format
+alias ls='ls -GFh'
+
+# Same as above, but in long listing format
+alias ll='ls -GFhl'
+
+alias rake='noglob rake'
+alias bower='noglob bower'
+
 alias reload!='. ~/.zshrc'
