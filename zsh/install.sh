4b9185f8133b49b73d197f23569798c04c41b73b
Michael Schaerfer <m.schaerfer@emakina.at>
2016-12-26 12:48:52 +0100

    install zsh-docker support install zsh-syntax-highl

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,6 @@
 mkdir -p ~/.zsh/completion
 
 curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
+curl -fLo ~/.zsh/completion/_docker https://raw.githubusercontent.com/docker/docker/master/contrib/completion/zsh/_docker
 
-curl -fLo ~/.zsh/completion/_docker https://raw.github.com/felixr/docker-zsh-completion/master/_docker
+brew install zsh-syntax-highlighting
