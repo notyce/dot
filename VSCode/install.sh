2be6576496ab21d006f86263f1d32e412d15064d
Michael Schaerfer <m.schaerfer@emakina.at>
2018-06-06 21:08:26 +0200

    added VSCode settings

---
+++ VSCode/install.sh
@@ -0,0 +1,3 @@
+ln -s /Users/shm/.dot/VSCode/settings.json /Users/shm/Library/Application\ Support/Code/User/settings.json
+ln -s /Users/shm/.dot/VSCode/keybindings.json /Users/shm/Library/Application\ Support/Code/User/keybindings.json
+ln -s /Users/shm/.dotfiles/VSCode/snippets/ /Users/shm/Library/Application\ Support/Code/User/snippets
